e0de16b3548631c82eff3e9c67215fe9d9660677
dddwmx <2757168787@qq.com>
2025-08-20 16:48:58 +0800

    引入Vant ui,设置好BasicalLayout

---
+++ src/layouts/BasicalLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nav-bar">
+    <van-nav-bar title="标题" left-text="返回" left-arrow>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
+  </div>
+  <div class="container"></div>
+  <div class="footer">
+    <van-tabbar v-model="active" @change="onChange">
+      <van-tabbar-item icon="home-o">主页</van-tabbar-item>
+      <van-tabbar-item icon="search">队伍</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">个人</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { showToast } from 'vant'
+const active = ref('home')
+const onChange = (index) => showToast(`标签 ${index}`)
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'vant/lib/index.css'
+import { Button } from 'vant'
+import { NavBar } from 'vant'
+import { Tabbar, TabbarItem } from 'vant'
+import { Icon } from 'vant'
 
 const app = createApp(App)
-
+app.use(Tabbar)
+app.use(TabbarItem)
+app.use(NavBar)
 app.use(createPinia())
 app.use(router)
-
+app.use(Button)
+app.use(Icon)
 app.mount('#app')
